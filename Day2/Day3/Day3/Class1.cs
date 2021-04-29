632312cc0e3d5e5efd415866f1b15fdc6e778b04
barleysack <hooker4020@naver.com>
2021-04-29 12:24:35 +0900

    before lunch

---
+++ Day2/Day3/Day3/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3
+{
+    class Testyme
+    {
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
--- Day2/Day3/Day3/Program.cs
+++ Day2/Day3/Day3/Program.cs
@@ -1,18 +1,62 @@
 ﻿using System;
-
+using System.Linq;
 namespace Day3
 {
     class Program
     {
+        
+        static void Main3(string[] args)
+        {
+            int i = 0;
+
+            while (i <= 10)
+            {
+                i = i + 1;
+                if (i % 2 == 1)
+                {
+                    continue;
+                }
+                
+                Console.WriteLine(i);
+
+            }
+        }
         static void Main(string[] args)
         {
-            for (int i= 1; i<=10; i++)
+            double[] a= { 100, 90, 91 };
+            double davg;
+            davg = Enumerable.Average(a);
+            Console.WriteLine(davg);
+            var lol = Enumerable.Range(1, 10);
+
+            Console.WriteLine(lol);
+            int k;
+            k = 7;
+            int p;
+            p = 9;
+            int answer=Testyme.Add(k, p);
+            Console.WriteLine(answer);
+
+           
+            int[] iscore;
+            iscore = new int[4];
+            for(int i=0; i<=3; i++)
             {
-                if (i % 2 == 0)
-                {
-                    Console.WriteLine(i);
-                }
+                
+                
+                iscore[i] = (i + 1) * 1000;
+               
             }
+            Console.WriteLine(iscore.Length);
+            
+            foreach(int i in iscore)
+            {
+                Console.WriteLine(i);
+            }
+
+
+
+
         }
     }
 }
