40722e6cc075e16e03d0a376a5a4e26d2c7a6c94
barleysack <hooker4020@naver.com>
2021-04-30 14:42:07 +0900

    little more

--- Day4_2/Day4_2/Program.cs
+++ Day4_2/Day4_2/Program.cs
@@ -1,12 +1,45 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace Day4_2
 {
-    class Program
+    class SmartFactory
     {
         static void Main(string[] args)
         {
+            Random rd = new Random();
             Console.WriteLine("Hello World!");
+            hello.Main2(args);
+            List<student> listybeaky = new List<student>();
+            
+            for ( int i= 1; i < 6; i++)
+            {
+                listybeaky.Add(new student() { name = "김"+i+"성", grade = rd.Next(1, 5) });
+            }
+            foreach ( var i in listybeaky)
+            {
+                Console.WriteLine(i.name + ":" + i.grade + "(이전)");
+            }
+            foreach (var item in listybeaky)
+            {
+                if (item.grade > 1)
+                {
+                    listybeaky.Remove(item);
+                    break;
+                }
+            }
+            foreach (var i in listybeaky)
+            {
+                Console.WriteLine(i.name + ":" + i.grade + "(이후)");
+            }
+        }
+        
+    }
+    class hello
+    {
+        public static void Main2(string[] args)
+        {
+            
+            Console.WriteLine("Which one will go first"); 
         }
     }
 }
