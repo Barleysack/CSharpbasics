6443390209ace7d82efd562a45e4b72a633c616e
barleysack <hooker4020@naver.com>
2021-05-03 10:48:16 +0900

    Added line to test VS commit

--- Day5_01/Day5_01/Program.cs
+++ Day5_01/Day5_01/Program.cs
@@ -7,6 +7,7 @@ namespace Day5_01
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.Write("long time no see");
         }
     }
 }
